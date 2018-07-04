0d4a4cef1cdab99997fbe04d0819f7f3ca2fb15b
ArduinoSec <tcelvis@gmail.com>
2018-07-04 19:27:06 +0000

    5 sec

--- Relay/Relay.ino
+++ Relay/Relay.ino
@@ -18,7 +18,7 @@ Serial.begin(9600);
    digitalWrite(RELAY1, 0);           // Turns ON Relays 1
    
    Serial.println("Light ON");
-   delay(5000);                       // Wait 2 seconds
+   delay(5000);                       // Wait 5 seconds
 
    digitalWrite(RELAY1, 1);           // Turns Relay Off
    Serial.println("Light OFF");
