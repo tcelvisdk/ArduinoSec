423859ca4a6a12b82e9f399a4e8928e2f31cdbef
Jesperrod <jesper.rodenberg@live.com>
2018-07-08 22:50:36 +0200

    Persistent storage

---
+++ Persist/Persist.ino
@@ -0,0 +1,98 @@
+
+#include "library\SerialDisplay.h"
+#include <EEPROM.h>
+
+const int MAXSTORAGE = 100;
+const int bClear = 2;
+const int bRead = 3;
+const int bWrite = 4;
+
+int addr = 0;
+String val;
+String _constData = "Data: ";
+String _constNowStoring = "Now storing: ";
+int bClearState;
+int bReadState;
+int bWriteState;
+
+
+void eepromClear()
+{
+  for (int i = 0 ; i < MAXSTORAGE ; i++) {
+    EEPROM.write(i, 0);
+  }
+}
+void eepromWriteString(int StartPosition, String inputStr)
+{
+  char *myArray;
+  myArray = inputStr.c_str();
+  int dataPosition = StartPosition;
+  for (int i = StartPosition ; ((myArray[i] != '\0') && (i < MAXSTORAGE)) ; i++) {
+    EEPROM.write(i, myArray[i]);
+    dataPosition++;
+  }
+  EEPROM.write(dataPosition, '\0');
+}
+
+String eepromReadString(int StartPosition)
+{
+  //String returnString;
+  char myArray[MAXSTORAGE];
+  int dataPosition = 0;
+  for (int i = StartPosition ; i < MAXSTORAGE ; i++)
+  {
+    myArray[dataPosition] = EEPROM.read(i);
+    dataPosition++;
+  }
+  String _returnString(myArray);
+  return _returnString;
+}
+
+
+void setup()
+{
+  Serial.begin(9600);
+  DisplayInitialize();
+  pinMode(bClear, INPUT);
+  pinMode(bRead, INPUT);
+  pinMode(bWrite, INPUT);
+  WriteString("Ready");
+  delay(5000);
+}
+
+void loop()
+{
+  bClearState = digitalRead(bClear);
+  bReadState = digitalRead(bRead);
+  bWriteState = digitalRead(bWrite);
+
+  if  ( bClearState == HIGH)
+  {
+    WriteString("Now clearing data");
+    delay(1000);
+    eepromClear();
+    WriteString("Data cleared");
+  }
+  else
+  {
+    if  ( bReadState == HIGH)
+    {
+      WriteString("Now reading data");
+      delay(1000);
+      String dataIn = eepromReadString(0);
+      WriteString("Data read: " + dataIn);
+    }
+    else
+    {
+      if  ( bWriteState == HIGH)
+      {
+        String inputData = Serial.readString();
+        delay(1000);
+        WriteString(_constNowStoring + inputData);
+        eepromWriteString(addr, inputData);
+        WriteString("Data stored");
+        
+      }
+    }
+  }
+}
--- TemperatureLM35/TemperatureLM35.ino
+++ TemperatureLM35/TemperatureLM35.ino
@@ -4,28 +4,22 @@ int ledPin = 8;      // select the pin for the LED
 int sensorValue = 0;  // variable to store the value coming from the sensor
 
 int get_temperature(int pin) {
-// We need to tell the function which pin the sensor is hooked up to. We're using
-// the variable pin for that above
-// Read the value on that pin
-int temperature = analogRead(pin);
-// Calculate the temperature based on the reading and send that value back
-float voltage = temperature * 5.0;
-voltage = voltage / 1024.0;
-return ((voltage - 0.5) * 100);
+  int temperature = analogRead(pin);
+  // Calculate the temperature based on the reading and send that value back
+  float voltage = temperature * 5.0;
+  voltage = voltage / 1024.0;
+  return ((voltage - 0.5) * 100);
 }
+
 void setup() {
   // declare the ledPin as an OUTPUT:
   pinMode(ledPin, OUTPUT);
   Serial.begin(9600);
-  
 }
 
 void loop() {
   // read the value from the sensor:
-  
-  
-   Serial.println(get_temperature(sensorPin));
-  
+  Serial.println(get_temperature(sensorPin));
   // turn the ledPin on
   digitalWrite(ledPin, HIGH);
   // stop the program for <sensorValue> milliseconds:
