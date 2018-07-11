332f3bdf5a73c3e75de47cda4c00495167063752
Jesperrod <jesper.rodenberg@live.com>
2018-07-11 22:01:01 +0200

    The clock

--- Clock/Clock.ino
+++ Clock/Clock.ino
@@ -1,25 +1,27 @@
 #include "Wire.h"
+#include "library\SerialDisplay.h"
 #define DS3231_I2C_ADDRESS 0x68
 // Convert normal decimal numbers to binary coded decimal
 byte decToBcd(byte val)
 {
-  return( (val/10*16) + (val%10) );
+  return ( (val / 10 * 16) + (val % 10) );
 }
 // Convert binary coded decimal to normal decimal numbers
 byte bcdToDec(byte val)
 {
-  return( (val/16*10) + (val%16) );
+  return ( (val / 16 * 10) + (val % 16) );
 }
 void setup()
 {
   Wire.begin();
   Serial.begin(9600);
+  DisplayInitialize();
   // set the initial time here:
   // DS3231 seconds, minutes, hours, day, date, month, year
-  // setDS3231time(30,42,21,4,26,11,14);
+  setDS3231time(00, 35, 21, 4, 11, 07, 18);
 }
 void setDS3231time(byte second, byte minute, byte hour, byte dayOfWeek, byte
-dayOfMonth, byte month, byte year)
+                   dayOfMonth, byte month, byte year)
 {
   // sets time and date data to DS3231
   Wire.beginTransmission(DS3231_I2C_ADDRESS);
@@ -34,12 +36,12 @@ dayOfMonth, byte month, byte year)
   Wire.endTransmission();
 }
 void readDS3231time(byte *second,
-byte *minute,
-byte *hour,
-byte *dayOfWeek,
-byte *dayOfMonth,
-byte *month,
-byte *year)
+                    byte *minute,
+                    byte *hour,
+                    byte *dayOfWeek,
+                    byte *dayOfMonth,
+                    byte *month,
+                    byte *year)
 {
   Wire.beginTransmission(DS3231_I2C_ADDRESS);
   Wire.write(0); // set DS3231 register pointer to 00h
@@ -55,22 +57,22 @@ byte *year)
   *year = bcdToDec(Wire.read());
 }
 void displayTime()
-{
+{ //String timeToView;
   byte second, minute, hour, dayOfWeek, dayOfMonth, month, year;
   // retrieve data from DS3231
   readDS3231time(&second, &minute, &hour, &dayOfWeek, &dayOfMonth, &month,
-  &year);
+                 &year);
   // send it to the serial monitor
   Serial.print(hour, DEC);
   // convert the byte variable to a decimal number when displayed
   Serial.print(":");
-  if (minute<10)
+  if (minute < 10)
   {
     Serial.print("0");
   }
   Serial.print(minute, DEC);
   Serial.print(":");
-  if (second<10)
+  if (second < 10)
   {
     Serial.print("0");
   }
@@ -81,29 +83,45 @@ void displayTime()
   Serial.print(month, DEC);
   Serial.print("/");
   Serial.print(year, DEC);
+  String timeToView = String(hour, DEC) 
+                    + String(":") 
+                    + ((minute<10)?String("0"):String(""))
+                    + String(minute, DEC) 
+                    + String(":") 
+                    + ((second<10)?String("0"):String(""))
+                    + String(second, DEC);
+  WriteLineOne(timeToView);                    
+            
+  String dateToView = String(dayOfMonth, DEC) 
+                    + String(".") 
+                    + String(month, DEC) 
+                    + String(".") 
+                    + String(year, DEC);                    
+  WriteLineTwo(dateToView);
+  
   Serial.print(" Day of week: ");
-  switch(dayOfWeek){
-  case 1:
-    Serial.println("Sunday");
-    break;
-  case 2:
-    Serial.println("Monday");
-    break;
-  case 3:
-    Serial.println("Tuesday");
-    break;
-  case 4:
-    Serial.println("Wednesday");
-    break;
-  case 5:
-    Serial.println("Thursday");
-    break;
-  case 6:
-    Serial.println("Friday");
-    break;
-  case 7:
-    Serial.println("Saturday");
-    break;
+  switch (dayOfWeek) {
+    case 1:
+      Serial.println("Sunday");
+      break;
+    case 2:
+      Serial.println("Monday");
+      break;
+    case 3:
+      Serial.println("Tuesday");
+      break;
+    case 4:
+      Serial.println("Wednesday");
+      break;
+    case 5:
+      Serial.println("Thursday");
+      break;
+    case 6:
+      Serial.println("Friday");
+      break;
+    case 7:
+      Serial.println("Saturday");
+      break;
   }
 }
 void loop()
